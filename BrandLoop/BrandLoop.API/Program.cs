8f8579342dbd4aef6d60556c44d5ab0ba5da78ac
tinse170533 <nguyentruongtin5@gmail.com>
2025-07-13 16:19:21 +0700

    up

--- BrandLoop/BrandLoop.API/Program.cs
+++ BrandLoop/BrandLoop.API/Program.cs
@@ -182,7 +182,7 @@ public class Program
             options.ListenAnyIP(7222); // HTTP
             options.ListenAnyIP(7443, listenOptions =>
             {
-                listenOptions.UseHttps("/app/https/brandloop.pfx", "1234");
+                listenOptions.UseHttps("/app/https/brandloop.io.vn.pfx", "12345");
             });
         });
 
