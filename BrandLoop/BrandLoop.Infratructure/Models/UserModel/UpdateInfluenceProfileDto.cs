6f2dc7879ed2030292630ff6955f3ea9c8975b49
cbq114 <nguyentruongtin5@gmail.com>
2025-07-18 14:33:56 +0700

    up

--- BrandLoop/BrandLoop.Infratructure/Models/UserModel/UpdateInfluenceProfileDto.cs
+++ BrandLoop/BrandLoop.Infratructure/Models/UserModel/UpdateInfluenceProfileDto.cs
@@ -10,45 +10,46 @@ namespace BrandLoop.Infratructure.Models.UserModel
     public class UpdateInfluenceProfileDto
     {
         [StringLength(100)]
-        public string Nickname { get; set; }
+        public string? Nickname { get; set; }
 
-        public string Bio { get; set; }
+        public string? Bio { get; set; }
 
         [StringLength(255)]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [StringLength(100)]
-        public string ContentCategory { get; set; }
+        public string? ContentCategory { get; set; }
 
         [StringLength(255)]
-        public string Location { get; set; }
+        public string? Location { get; set; }
 
         [StringLength(255)]
-        public string Languages { get; set; }
+        public string? Languages { get; set; }
 
 
         [StringLength(255)]
-        public string PortfolioUrl { get; set; }
+        public string? PortfolioUrl { get; set; }
 
         [StringLength(255)]
-        public string Facebook { get; set; }
+        public string? Facebook { get; set; }
 
         [StringLength(255)]
-        public string Instagram { get; set; }
+        public string? Instagram { get; set; }
 
         [StringLength(255)]
-        public string Tiktok { get; set; }
+        public string? Tiktok { get; set; }
 
         [StringLength(255)]
-        public string Youtube { get; set; }
+        public string? Youtube { get; set; }
 
         [StringLength(10)]
-        public string Gender { get; set; }
+        public string? Gender { get; set; }
 
         public DateOnly? DayOfBirth { get; set; }
 
         [StringLength(20)]
-        public string Phone { get; set; }
-        public string FullName { get; set; }
+        public string? Phone { get; set; }
+        public string? FullName { get; set; }
+        public string? ProfileImage { get; set; }
     }
 }
