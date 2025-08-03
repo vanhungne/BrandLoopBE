1c85f248f758459f95faed3bd5cf2227e782a07b
cbq114 <nguyentruongtin5@gmail.com>
2025-08-03 18:43:28 +0700

    up

--- BrandLoop/BrandLoop.API/Program.cs
+++ BrandLoop/BrandLoop.API/Program.cs
@@ -119,8 +119,7 @@ public class Program
             options.AddPolicy("AllowSpecificOrigin", policy =>
             {
                 policy.WithOrigins("http://localhost:8880", "https://159.223.70.240:2053", "https://localhost:2053"
-                    , "https://brandloop.io.vn", "https://www.brandloop.io.vn", "https://139.59.226.2:2053", "http://localhost:5173"
-                    , "https://brandloop.pages.dev")
+                    , "https://brandloop.io.vn", "https://www.brandloop.io.vn")
                       .AllowAnyHeader()
                       .AllowAnyMethod()
                       .AllowCredentials();
