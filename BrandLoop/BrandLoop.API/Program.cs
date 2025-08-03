71890c6f55db9a2d9f09f08085332eb3908cfd40
cbq114 <nguyentruongtin5@gmail.com>
2025-08-03 17:02:22 +0700

    up

--- BrandLoop/BrandLoop.API/Program.cs
+++ BrandLoop/BrandLoop.API/Program.cs
@@ -118,7 +118,7 @@ public class Program
         {
             options.AddPolicy("AllowSpecificOrigin", policy =>
             {
-                policy.WithOrigins("http://localhost:8880", "https://128.199.174.77:2053", "https://localhost:2053"
+                policy.WithOrigins("http://localhost:8880", "https://159.223.70.240:2053", "https://localhost:2053"
                     , "https://brandloop.io.vn", "https://www.brandloop.io.vn", "https://139.59.226.2:2053", "http://localhost:5173"
                     , "https://brandloop.pages.dev")
                       .AllowAnyHeader()
@@ -178,14 +178,14 @@ public class Program
 
         //config ure Kestrel server to listen on specific ports
 
-        //builder.WebHost.ConfigureKestrel(options =>
-        //{
-        //    options.ListenAnyIP(8880); // HTTP
-        //    options.ListenAnyIP(2053, listenOptions =>
-        //    {
-        //        listenOptions.UseHttps("/app/https/brandloop.io.vn.pfx", "12345");
-        //    });
-        //});
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.ListenAnyIP(8880); // HTTP
+            options.ListenAnyIP(2053, listenOptions =>
+            {
+                listenOptions.UseHttps("/app/https/brandloop.io.vn.pfx", "12345");
+            });
+        });
 
 
         // Set default time zone
